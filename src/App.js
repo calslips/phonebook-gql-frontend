e4c4290841d8c1f71155411585943896043aa81b
Caleb Phillips <csphillips5@gmail.com>
2021-10-18 16:02:54 -0400

    8-1 implement main view of app with useQuery hook to make allPersons query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-function App() {
+const ALL_PERSONS = gql`
+query {
+  allPersons {
+    name
+    phone
+    id
+  }
+}
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
   return (
     <div>
+      {result.data.allPersons.map((p) => p.name).join(', ')}
     </div>
   );
 }
